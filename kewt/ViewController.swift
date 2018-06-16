3a029da09f84b7dcdbd92ef652b6c1b06b9a41c3
Arif Mulya <13158642+armul@users.noreply.github.com>
2018-06-16 09:34:08 +0700

    Initial Commit

---
+++ kewt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  kewt
+//
+//  Created by Arif Mulya on 16/06/18.
+//  Copyright © 2018 Arif Mulya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
